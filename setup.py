583dc567ca133646290f4fe3c8e12ba372126d0a
Nathan Raw <nxr9266@g.rit.edu>
2025-07-04 19:28:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import find_packages, setup
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='encoded-video',
     packages=find_packages(exclude=['examples']),
@@ -12,5 +9,9 @@ setup(
     author='Nathan Raw',
     author_email='naterawdata@gmail.com',
     url='https://github.com/nateraw/encoded-video',
-    install_requires=requirements,
+    install_requires=[
+        "av>=14.0.1",
+        "iopath",
+        "numpy",
+    ]
 )
